06fc902829e625fc8bdf69b47d358451af3040e8
Markus <dontsendmeemails>
2013-04-16 18:43:52 +0200

    minor change

--- create_ycsbtable.sh
+++ create_ycsbtable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp /home/clex/create_ycsbtable /home/clex/create_ycsbtable_tmp
-sed -i -e "s|# 'replication_factor':X|'replication_factor':$1|" /home/clex/create_ycsbtable_tmp
+sed -i -e "s|X|$1|" /home/clex/create_ycsbtable_tmp
 cqlsh -f /home/clex/create_ycsbtable_tmp
 rm /home/clex/create_ycsbtable_tmp

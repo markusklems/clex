a79f6d82980c116444016073094a4dce807faf16
Markus <dontsendmeemails>
2013-05-13 14:24:03 +0200

    Minor changes of snapshot / restore scripts

--- remove_data.sh
+++ remove_data.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #cqlsh -f /home/clex/drop_ycsbtable
-rm -Rf /var/lib/cassandra/*
-mkdir -p /var/lib/cassandra/data
-mkdir -p /var/lib/cassandra/commitlog
-mkdir -p /var/lib/cassandra/saved_caches
-chown cassandra -R /var/lib/cassandra
+nodetool invalidatekeycache
+nodetool invalidaterowcache
+nodetool clearsnapshot
+(echo "DROP KEYSPACE usertable;" > /tmp/dropkeyspace; cqlsh -f /tmp/dropkeyspace; rm /tmp/dropkeyspace)
+#rm -Rf /var/lib/cassandra/*
+#mkdir -p /var/lib/cassandra/data
+#mkdir -p /var/lib/cassandra/commitlog
+#mkdir -p /var/lib/cassandra/saved_caches
+#chown cassandra -R /var/lib/cassandra
 exit
--- restore_snapshot.sh
+++ restore_snapshot.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 cp /var/lib/backupusertable/* /var/lib/cassandra/data/usertable/data/.

de3c670f26860fad922339c126a26fe812566470
Markus <dontsendmeemails>
2013-05-14 13:53:57 +0200

    Install git. Then download the git repo and bootstrap everything else from the scripts.

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+if which dpkg &> /dev/null; then
+  sudo apt-get install -y git 
+elif which rpm &> /dev/null; then
+  sudo yum install -y git --disablerepo=epel
+fi
+
 (cd /home; git clone git://github.com/markusklems/clex.git)

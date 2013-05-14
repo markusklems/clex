2883623986e50e81d6639c73b742e5f8b708ea84
Markus <dontsendmeemails>
2013-05-14 16:22:09 +0200

    Multi DC script fix

--- cassandra/multidc_config.sh
+++ cassandra/multidc_config.sh
@@ -12,6 +12,6 @@ sed -i -e "s|endpoint_snitch: SimpleSnitch|endpoint_snitch: PropertyFileSnitch|"
 topology_file="/etc/cassandra/cassandra-topology.properties"
 if [ -f /etc/cassandra/conf/cassandra-topology.properties ]; then
   topology_file="/etc/cassandra/conf/cassandra-topology.properties"
+  sudo rm "$topology_file"
 fi
-sudo rm "$topology_file"
 sudo cp "/home/clex/cassandra/$mytopology" "$topology_file"

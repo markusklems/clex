9c3a1f380894f11f4ec6a7d8a5dc9530d0a8c6c2
Markus <dontsendmeemails>
2013-06-22 13:12:18 +0200

    Fix

--- monitoring/nagios_ubuntu_install.sh
+++ monitoring/nagios_ubuntu_install.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y nagios3
 # setup remote monitoring for the cluster
 sudo apt-get -y install nagios3 nagios-nrpe-plugin
 # pssh into the node servers and install the agents
-sudo parallel-ssh -h ../hosts.txt -l $USER -o /tmp/nagios-install 'sudo apt-get -y install nagios-nrpe-server; sudo sed -i.bak -r -e \'s/allowed_hosts=127.0.0.1/allowed_hosts=$NAGIOS_HOST_IP/g\' /etc/nagios/nrpe.cfg; sudo /etc/init.d/nagios-nrpe-server restart'
+sudo parallel-ssh -h ../hosts.txt -l $USER -o /tmp/nagios-install "sudo apt-get -y install nagios-nrpe-server; sudo sed -i.bak -r -e 's/allowed_hosts=127.0.0.1/allowed_hosts=$NAGIOS_HOST_IP/g' /etc/nagios/nrpe.cfg; sudo /etc/init.d/nagios-nrpe-server restart"

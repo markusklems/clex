79fc90127106f8de612f61fe13d888d196b0b341
Markus <dontsendmeemails>
2013-06-22 13:20:04 +0200

    use the cassandra key for the specified hosts

--- setup_keys.sh
+++ setup_keys.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 USER=root
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa_cassandra
+KEY_NAME=~/.ssh/id_rsa_cassandra
+ssh-keygen -t rsa -N "" -f $KEY_NAME
 while read h; do
   echo "Copy public key to $h"
-  cat ~/.ssh/id_rsa_cassandra.pub | ssh "$USER@$h" "mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"
+  cat "$KEY_NAME.pub" | ssh "$USER@$h" "mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"
+ echo "Host $h
+    IdentityFile $KEY_NAME" | sudo tee -a ~/.ssh/config
 done < hosts.txt

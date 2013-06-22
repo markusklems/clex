f67463c5f39ddca3de829f8ef88d12e2a6797421
Markus <dontsendmeemails>
2013-06-22 11:44:27 +0200

    Bugfix

--- monitoring/nagios_ubuntu_config.sh
+++ monitoring/nagios_ubuntu_config.sh
@@ -2,7 +2,7 @@
 # http://askubuntu.com/questions/145518/how-do-i-install-nagios
 sudo sed -i -e "s|- check_external_commands=0|- check_external_commands=1|" /etc/nagios3/nagios.cfg
 sudo service apache2 restart
-sudo sed -i -e '/^nagios:x:/ s/$/ :www-data/' /etc/group
+sudo sed -i -e '/^nagios:x:/ s/$/www-data/' /etc/group
 sudo chmod g+x /var/lib/nagios3/rw
 sudo chmod g+x /var/lib/nagios3
 sudo service nagios3 restart

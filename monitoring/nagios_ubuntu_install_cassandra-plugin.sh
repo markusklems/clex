8c3c82c480b54f681f4ac8aecb3eefbfb51c1c22
Markus <dontsendmeemails>
2013-06-22 16:12:42 +0200

    Must also make commands known on the remote hosts.

--- monitoring/nagios_ubuntu_install_cassandra-plugin.sh
+++ monitoring/nagios_ubuntu_install_cassandra-plugin.sh
@@ -9,7 +9,7 @@ sudo parallel-ssh -h $HOSTS_FILE -l $USER -o /tmp/nagios-install "sudo apt-get -
 
 # install jmx plugin for access to all jmx info
 (wget http://downloads.sourceforge.net/project/nagioscheckjmx/nagioscheckjmx/1.0/check_jmx.tar.gz; tar xvfz check_jmx.tar.gz; mv check_jmx /usr/lib/nagios/plugins/)
-sudo parallel-ssh -h $HOSTS_FILE -l $USER -o /tmp/nagios-jmx-plugin "(wget http://downloads.sourceforge.net/project/nagioscheckjmx/nagioscheckjmx/1.0/check_jmx.tar.gz; tar xvfz check_jmx.tar.gz; mv check_jmx /usr/lib/nagios/plugins/)"
+sudo parallel-ssh -h $HOSTS_FILE -l $USER -o /tmp/nagios-jmx-plugin "(wget http://downloads.sourceforge.net/project/nagioscheckjmx/nagioscheckjmx/1.0/check_jmx.tar.gz; tar xvfz check_jmx.tar.gz; mv check_jmx /usr/lib/nagios/plugins/); cp /home/clex/monitoring/nagios_cassandra_command.cfg /etc/nagios-plugins/config/cassandra_cmd.cfg"
 
 # install a simpler nagios-cassandra plugin as an alternative
 # sudo apt-get install libwww-perl libjson-perl
